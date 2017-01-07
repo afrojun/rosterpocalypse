54ce1cde2a87790d18211de689894f1b96a30f2c
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-01-07 23:22:18 +0200

    Use Tournament::GLOBAL_REGION when referring to the 'Global' region name.

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module FantasyHots
     # -- all .rb files in that directory are automatically loaded.
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/lib)
   end
 end
