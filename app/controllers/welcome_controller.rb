950c3a602ec4bbe77af9eeb5625aa6ecb47ab85a
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-01-10 14:35:30 +0200

    Let's Encrypt config [take 2].

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
   end
 
   def letsencrypt
-    render text: "64y4wzM9825Wk03lCfiB8qrz7tjP-w9470FY7sxOL4U.Gw5TZZlZX9cQX1gHyGpt_NSCFx9i0l_VQKEE99-44CY"
+    render text: "_O17yumSO2MxMnAK0FDoHTZ4QgHIrDrSB5Pp8AAk3g0.Gw5TZZlZX9cQX1gHyGpt_NSCFx9i0l_VQKEE99-44CY"
   end
 end
