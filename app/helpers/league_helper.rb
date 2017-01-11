da910eebf9f5c63cf9b9ef415c1954a787df9261
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-01-12 00:26:24 +0200

    Improve some League views.

--- app/helpers/league_helper.rb
+++ app/helpers/league_helper.rb
@@ -1,26 +1,26 @@
 module LeagueHelper
 
-  def league_membership_change league
-    roster = Roster.find_by_manager_and_region(current_user.manager, league.tournament.region)
+  def league_membership_change league, button_size_class
+    roster = Roster.find_by_manager_and_tournament(current_user.manager, league.tournament)
     if league.rosters.include? roster
-      link_to_leave_league league
+      link_to_leave_league league, button_size_class
     else
-      link_to_join_league league
+      link_to_join_league league, button_size_class
     end
   end
 
-  def link_to_join_league league
-    link_to_manage_league_membership league, :join
+  def link_to_join_league league, button_size_class
+    link_to_manage_league_membership league, :join, button_size_class
   end
 
-  def link_to_leave_league league
-    link_to_manage_league_membership league, :leave
+  def link_to_leave_league league, button_size_class
+    link_to_manage_league_membership league, :leave, button_size_class
   end
 
-  def link_to_manage_league_membership league, action
+  def link_to_manage_league_membership league, action, button_size_class
     join_path = "#{action}_#{league.class.to_s.underscore}_path"
-    button_class = action.to_sym == :join ? "btn-info" : "btn-danger"
-    link_to "#{action.to_s.titleize}", send(join_path.to_sym, league), method: :post, class: "btn btn-sm #{button_class}"
+    button_colour_class = action.to_sym == :join ? "btn-info" : "btn-danger"
+    link_to "#{action.to_s.titleize}", send(join_path.to_sym, league), method: :post, class: "btn #{button_colour_class} #{button_size_class}"
   end
 
 end
