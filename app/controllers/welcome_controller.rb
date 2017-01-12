fc0bf26a53979ae6b54cf2697455126c544c57de
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-01-12 04:56:32 +0200

    Creating a new SSL certificate, updating letsencrypt check code.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
   end
 
   def letsencrypt
-    render text: "_O17yumSO2MxMnAK0FDoHTZ4QgHIrDrSB5Pp8AAk3g0.Gw5TZZlZX9cQX1gHyGpt_NSCFx9i0l_VQKEE99-44CY"
+    render text: "f9viIP8tRet6b40cNIbQs7AFJYHxXHdFKXxH_yuwvEQ.Gw5TZZlZX9cQX1gHyGpt_NSCFx9i0l_VQKEE99-44CY"
   end
 end
