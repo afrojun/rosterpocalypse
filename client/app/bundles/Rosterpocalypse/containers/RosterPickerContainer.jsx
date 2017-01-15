d9756be89caaf3be380fe1f97105c18385e924c9
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-01-15 23:22:24 +0530

    Also show the max points where the total score is displayed on the RosterPicker.

--- client/app/bundles/Rosterpocalypse/containers/RosterPickerContainer.jsx
+++ client/app/bundles/Rosterpocalypse/containers/RosterPickerContainer.jsx
@@ -147,7 +147,7 @@ class RosterPickerContainer extends React.Component {
 
     let roundedTotal = Math.round(total * 100)/100;
     let colour = (roundedTotal > this.props.maxRosterValue ? "red" : "");
-    return(this.colourText(roundedTotal, colour));
+    return(this.colourText(roundedTotal + "/" + this.props.maxRosterValue, colour));
   }
 
   rosterLockStatus() {
@@ -254,7 +254,7 @@ class RosterPickerContainer extends React.Component {
             {this.state.roster.name}
           </h2>
           <p>
-            Total value: <b>{this.state.roster.players[0] ? this.totalValue() : "0"}</b>
+            Total value: <b>{this.state.roster.players[0] ? this.totalValue() : "0/" + this.props.maxRosterValue}</b>
             <br/>
             <b>{this.state.roster.current_gameweek.roster_lock_date && this.rosterLockStatus()}</b>
           </p>
