9eb7d0e9bd6632591a07d0f353d7480611e5e566
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-06-06 14:07:52 +0200

    Set the host config option when connecting to the PostgreSQL DB from Sidekiq.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,19 +7,26 @@ if Rails.env.production?
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV['REDISCLOUD_URL'], size: 22 }
 
-    Rails.application.config.after_initialize do
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      ActiveRecord::Base.connection_pool.disconnect!
+    database_url = ENV['DATABASE_URL']
 
-      ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        #config['pool'] = 16
-        ActiveRecord::Base.establish_connection(config)
+    if database_url
+      Rails.application.config.after_initialize do
+        Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+        ActiveRecord::Base.connection_pool.disconnect!
 
-        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+        ActiveSupport.on_load(:active_record) do
+          config = Rails.application.config.database_configuration[Rails.env]
+          config['host'] = database_url
+          config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+          config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
+          #config['pool'] = 16
+          ActiveRecord::Base.establish_connection(config)
+
+          Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+        end
       end
+    else
+      raise "Unable to determine the Database URL. Bailing out."
     end
   end
 
