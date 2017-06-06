2c524ffb8f0be09beb1f223e8ea9569bda146f9d
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-06-06 13:11:19 +0200

    Tweak Redis connection numbers.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISCLOUD_URL'], size: 20 }
+    config.redis = { url: ENV['REDISCLOUD_URL'], size: 25 }
 
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
@@ -14,8 +14,7 @@ if Rails.env.production?
       ActiveSupport.on_load(:active_record) do
         config = Rails.application.config.database_configuration[Rails.env]
         config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 16
+        config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
         ActiveRecord::Base.establish_connection(config)
 
         Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
