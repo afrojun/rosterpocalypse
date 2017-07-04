8e977c3c2b53d329772c0053676a0065321859f8
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-07-04 13:04:51 +0200

    Add Csvable to Transfers

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,7 +1,18 @@
 class Transfer < ApplicationRecord
+  include Csvable
+
   belongs_to :gameweek_roster
   has_one :gameweek, through: :gameweek_roster
   has_one :roster, through: :gameweek_roster
   belongs_to :player_in, class_name: "Player"
   belongs_to :player_out, class_name: "Player"
+
+  def self.csv_collection
+    all.includes(:player_in, :player_out, :gameweek, roster: [:manager])
+  end
+
+  def self.csv_attributes
+    attributes = %w{id player_in.slug player_out.slug roster.manager.slug gameweek.start_date gameweek.end_date created_at}
+  end
+
 end
