59bc9bacd5b37693226cf6736aaafc93e59fbc65
Arjun Radhakrishnan <arj.rdh@gmail.com>
2017-07-07 03:45:53 +0200

    Add match_id to Game#edit form.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -102,7 +102,7 @@ class GamesController < RosterpocalypseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
-      params.require(:game).permit(:map_id, :start_date, :duration_s, :game_hash)
+      params.require(:game).permit(:map_id, :start_date, :duration_s, :game_hash, :match_id)
     end
 
     # We use the presenter to set this from the view
