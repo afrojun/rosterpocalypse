b9f41accc3c6a00d1a5558b4fe98cbc3bd2e4545
Arjun Radhakrishnan <arjun@datacamp.com>
2019-12-30 03:18:59 +0000

    Upgraded Rails to 5.2

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,9 @@ module FantasyHots
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.autoload_paths += %W[#{config.root}/lib]
